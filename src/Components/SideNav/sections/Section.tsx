f1816c1e3b6ecb85d69ab61d9d289cd75cd86902
Dan Stronge <dnlstronge@gmail.com>
2023-05-27 11:02:49 +0100

    [x] - create dummy SectionData [x] - create datamodel and export

---
+++ src/Components/SideNav/sections/Section.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import classes from "./Section.module.css"
+
+const Section = () => {
+
+}
+
+export default Section; 
---
+++ src/Components/SideNav/sections/SectionData.ts
@@ -0,0 +1,19 @@
+export type SectionDataModel = {
+    id: number,
+    heading: string,
+    subheadings: string[]
+} []
+
+
+export const data: SectionDataModel = [
+    {
+    id: 1,
+    heading: "Bloopity test",
+    subheadings: ["testing the datamodel", "Testing it good"]
+    },
+    {
+        id: 2,
+    heading: "Lets type test",
+    subheadings: ["testing the datamodel", "Testing it good"]
+    }
+]
