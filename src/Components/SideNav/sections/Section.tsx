2c14176be64f1b87aeb07afce0f21066a12fb218
Dan Stronge <dnlstronge@gmail.com>
2023-05-27 11:06:45 +0100

    [x] - add handler for drop [x] - add state

--- src/Components/SideNav/sections/Section.tsx
+++ src/Components/SideNav/sections/Section.tsx
@@ -1,8 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import classes from "./Section.module.css"
 
-const Section = () => {
-
+const Section = (props) => {
+    const [showDropDown, setShowDropDown] = useState(false)
+    const handleDropDown = () => {
+        setShowDropDown(!showDropDown)
+    }
+    return (
+        <div className={classes.container}>
+            <h4 onClick={handleDropDown}>Dummy Header</h4>
+        </div>
+    )
 }
 
 export default Section; 
