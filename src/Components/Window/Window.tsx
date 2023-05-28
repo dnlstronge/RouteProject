b38b18bdff4432380d759f3b874e8784c928f273
Dan Stronge <dnlstronge@gmail.com>
2023-05-28 09:23:43 +0100

    [x] - add comment

--- src/Components/Window/Window.tsx
+++ src/Components/Window/Window.tsx
@@ -1,6 +1,9 @@
 import React from "react"
 import classes from "./Window.module.css"
 
+
+// window panel which displays section selected, get state from props
+
 const Window: React.FC = () => {
     return (
         <div className={classes.container}></div>
