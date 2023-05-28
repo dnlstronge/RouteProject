2b7b4a1e7fb412dcd8302c4fc369149558d6b4e9
Dan Stronge <dnlstronge@gmail.com>
2023-05-28 08:45:29 +0100

    [x] - create data type

--- src/Components/Data/Projects.ts
+++ src/Components/Data/Projects.ts
@@ -1,12 +1,28 @@
 import groupTaskImage from "../../Assets/ProjectScreens/GroupTask/1001a.png"
 
 
+//  migrate data to DB w/ FB backend to begin with
+
+type projectDataType = 
+    {
+    id: number,
+    name: string,
+    description: string,
+    screenShot: string,
+    liveDemo: string
+    gitHub: string
+     } []
+
+
 const projectData = [
   {
      name: 'Task Manager Application',
      description: 
-            "Task manager application built with React and using firebase to handle the backend.",
-     screenShot: groupTaskImage  
+            `Project management app built with React, allows different users to log in and exchange messages, ideas and update tasks etc.
+             Uses firebase for authentication and to accommodate CRUD operations`,
+     screenShot: groupTaskImage,
+     liveDemo: "",
+
   },
 
 
---
+++ src/Models/projectDataTypes.ts
@@ -0,0 +1,9 @@
+export type projectDataType = 
+{
+id: number,
+name: string,
+description: string,
+screenShot: string,
+liveDemo: string
+gitHub: string
+ } []
