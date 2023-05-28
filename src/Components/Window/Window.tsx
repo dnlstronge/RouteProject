591bb4125c3df33894cc4f9974f6b6a3cfadc469
Dan Stronge <dnlstronge@gmail.com>
2023-05-28 09:22:17 +0100

    [x] - add comp logic

---
+++ src/Components/Window/Window.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import classes from "./Window.module.css"
+
+const Window: React.FC = () => {
+    return (
+        <div className={classes.container}></div>
+    )
+}
+
+export default Window;
