e183accbf610c2c5943a2a2f7207dd40660c684b
Dan Stronge <dnlstronge@gmail.com>
2023-05-28 08:50:29 +0100

    [x] - implement type

--- src/Components/Data/Projects.ts
+++ src/Components/Data/Projects.ts
@@ -1,27 +1,20 @@
 import groupTaskImage from "../../Assets/ProjectScreens/GroupTask/1001a.png"
-
+import { projectDataType } from "../../Models/projectDataTypes"
 
 //  migrate data to DB w/ FB backend to begin with
 
-type projectDataType = 
-    {
-    id: number,
-    name: string,
-    description: string,
-    screenShot: string,
-    liveDemo: string
-    gitHub: string
-     } []
 
 
-const projectData = [
+export const projectData: projectDataType  = [
   {
+     id: 1001,
      name: 'Task Manager Application',
      description: 
             `Project management app built with React, allows different users to log in and exchange messages, ideas and update tasks etc.
              Uses firebase for authentication and to accommodate CRUD operations`,
      screenShot: groupTaskImage,
-     liveDemo: "",
+     liveDemo: "https://grouptaskapp-4eccd.web.app/",
+     gitHub: "https://github.com/dnlstronge/groupTask"
 
   },
 
