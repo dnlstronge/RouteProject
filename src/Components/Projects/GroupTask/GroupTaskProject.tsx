94183dc671e4f8485960711b31bf28985bb12dd0
Dan Stronge <dnlstronge@gmail.com>
2023-05-28 09:29:44 +0100

    [x] - hook up project data

---
+++ src/Components/Projects/GroupTask/GroupTaskProject.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import classes from "./GroupTaskProject.module.css"
+
+
+const GroupTaskProject: React.FC = () => {
+    return (
+        <div></div>
+    )
+}
--- src/Components/Window/Window.tsx
+++ src/Components/Window/Window.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import classes from "./Window.module.css"
-
+import { projectData } from "../Data/Projects"
 
 // window panel which displays section selected, get state from props
 
+const groupTaskData = projectData[0]
+
 const Window: React.FC = () => {
     return (
-        <div className={classes.container}></div>
+        <div className={classes.container}>
+            
+        </div>
     )
 }
 
