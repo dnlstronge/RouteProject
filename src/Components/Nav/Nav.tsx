90694f94c7a464cb70068b976371d8617646bcb1
Dan Stronge <dnlstronge@gmail.com>
2023-05-29 08:26:19 +0100

    [x] - add props

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import classes from "./Nav.module.css"
+import NavBtn from "./NavBtn";
 
 
 const Nav = () => {
+
+  const testHandler = () => {
+    console.log("test button function!!!")
+  }
+
+
   return (
-    <div className={classes.container}></div>
+    <div className={classes.container}>
+      <section className={classes.navButtons}>
+        <NavBtn text="Test click" onClick={testHandler} />
+      </section>
+    </div>
   )
 }
 
