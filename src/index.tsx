64f0ad73a553ac550dfe7777e8eac16fef52d17b
Dan Stronge <dnlstronge@gmail.com>
2023-05-30 09:04:50 +0100

    [x] - wrap app in provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { store } from "./Store/store"
 import './index.css';
 import App from './App';
 import Nav from './Components/Nav/Nav';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <Nav />
-    <App />
+    <Provider store={store}>
+       <Nav />
+       <App />
+    </Provider>
+   
   </React.StrictMode>
 );
