5676ae50c6ff47cedd270cbb989a2ca693fb57b8
Dan Stronge <dnlstronge@gmail.com>
2023-06-02 08:57:33 +0100

    [x] - create compoments to test approach for implementing icons to each project in window

---
+++ src/Components/Pages/TESTCOMP.tsx
@@ -0,0 +1,11 @@
+const TESTCOMP = () => {
+    return (
+        <div></div>
+    )
+}
+
+type propsToTest = {
+    icons: string[]
+}
+
+export default TESTCOMP
---
+++ src/Components/Pages/TESTPROPS.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const TESTPROPS: React.FC = () => {
+    return (
+        <div></div>
+    )
+}
+export default TESTPROPS;
+
+  
